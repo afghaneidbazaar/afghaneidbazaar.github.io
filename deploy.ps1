775f41b1a14fea84c8cd9d8b1e21c105fef36061
Abrahim Safi <abrahimsafi2021@github.com>
2026-03-27 10:54:30 -0700

    Initialize website files

---
+++ deploy.ps1
@@ -0,0 +1,6 @@
+git config user.email "abrahimsafi2021@github.com"
+git config user.name "Abrahim Safi"
+git add .
+git commit -m "Initialize website files"
+git branch -M main
+git push -u origin main
